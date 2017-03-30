f76d338e034ae4c02297b5e0ad964a89401b63bc
Suvedh Srikanth <suvedhs@gmail.com>
2017-03-29 22:16:06 -0500

    Modified menu to give either person statistics or group statistics depending on prompt

--- main.py
+++ main.py
@@ -2,7 +2,8 @@ import requests
 
 api_token = ""  # this is a global variable that stores the API token
 group_number = -1
-group_data = []
+groups_data = []
+data = []
 
 #TODO: Encrypt Token
 """ This involves getting api token from user, encrypt it, and save in a file.
@@ -12,19 +13,17 @@ Should check if file contains encrypted key and prompt user if not.
 #Prompts user to input developer API key
 def get_api_token():
     global api_token
-
+    global data
     api_token = str(raw_input("Enter your GroupMe developer API token:"))
+    response = requests.get('https://api.groupme.com/v3/groups?token=' + api_token)
+    data = response.json()
+    if data['response'] is None:
+        print "Invalid API Key!"
+        get_api_token()
 
 #Prints all of a user's groupme groups
 def print_groups():
 
-    response = requests.get('https://api.groupme.com/v3/groups?token=' + api_token)
-    data = response.json()
-    while data['response'] is None:
-        print "Invalid API Key!"
-        get_api_token()
-        response = requests.get('https://api.groupme.com/v3/groups?token=' + api_token)
-        data = response.json()
     if len(data['response']) == 0:
         print("You are not part of any groups.")
         return
@@ -40,12 +39,34 @@ def print_groups():
 The exact content of the pages is explained in the high level design document.
 """
 def menu():
-    get_group_number()
-    group_id = get_group_id(groups_data, group_number)
+
+    if chose_group():
+        print_groups()
+        get_group_number()
+        group_id = get_group_id(groups_data, group_number)
+    else :
+        print("oh shit")
 
 """ get_group_number retrieves the group number. It uses recursion to prompt user
 to input until a valid input is given.
 """
+def chose_group():
+    try:
+        print("Which type of statistic would you like to retrieve?")
+        print("1 - Group Specific Statistics")
+        print("2 - Person Specific Statistics")
+        choice = int(raw_input("Enter the number of the type of statistic: "))
+        print("")
+        if choice==1 :
+            return True
+        elif choice==2 :
+            return False
+        else :
+            print("Not a valid integer\n")
+            return chose_group()
+    except ValueError:
+        print("Not an integer\n")
+        return chose_group()
 def get_group_number():
     try:
         global group_number
@@ -75,5 +96,4 @@ if __name__ == '__main__':
     print('If you have not done so already, go to the following website to receive your API token: ' +
           'https://dev.groupme.com/. When signing up, it does not matter what you put for the callback URL')
     get_api_token()
-    print_groups()
     menu()
